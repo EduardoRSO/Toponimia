ca219cb11dcc482e5f9ccdf3d18042c301a4c613
erso <edusillva784@gmail.com>
2024-08-31 17:20:58 -0300

    Atualizei LoggingHandler para mostrar parametros ou não

--- dataframe/src/logging_logic/logging_handler.py
+++ dataframe/src/logging_logic/logging_handler.py
@@ -10,10 +10,13 @@ class LoggingHandler:
         self.logger.setLevel(logging.INFO)
     
     @staticmethod
-    def log_method(class_name, method_name, show_output=False):
+    def log_method(class_name, method_name, show_output=False, show_parameters=False):
         def decorator(func):
             def wrapper(self, *args, **kwargs):
-                parameters = f"Parameters: args={args}, kwargs={kwargs}"
+                if show_parameters:
+                    parameters = f"Parameters: args={args}, kwargs={kwargs}"
+                else:
+                    parameters = ""
                 logging.getLogger(class_name).info(parameters, extra={'class_name': class_name, 'method_name': method_name})
                 result = func(self, *args, **kwargs)
                 if show_output:
