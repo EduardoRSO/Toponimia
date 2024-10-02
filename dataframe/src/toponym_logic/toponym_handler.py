e2e940cd34b100b47d46592f592e44fd5d757841
erso <edusillva784@gmail.com>
2024-10-02 12:03:53 -0300

    implementei toponym_handler

---
+++ dataframe/src/toponym_logic/toponym_handler.py
@@ -0,0 +1,37 @@
+import spacy
+from spacy.matcher import Matcher
+from logging_logic.logging_handler import LoggingHandler
+
+class ToponymHandler(LoggingHandler):
+    def __init__(self, directory_name):
+        super().__init__(directory_name)
+        self.directory_name = directory_name
+        self.nlp = spacy.load("pt_core_news_sm")
+        self.matcher = Matcher(self.nlp.vocab)
+        self._add_patterns()
+
+    def _add_patterns(self):
+        patterns = [
+            [{"POS": "PROPN"}],  # Ex: São Paulo
+            [{"POS": "PROPN"}, {"POS": "ADP"}, {"POS": "PROPN"}],  # Ex: Rio de Janeiro
+            [{"POS": "NOUN"}, {"POS": "ADP"}, {"POS": "PROPN"}],  # Ex: Cidade de São Paulo
+            [{"POS": "NOUN"}, {"POS": "ADJ"}, {"POS": "ADP"}, {"POS": "PROPN"}],  # Ex: Vila Nova de Gaia
+            [{"POS": "PROPN"}, {"POS": "CCONJ"}, {"POS": "PROPN"}],  # Ex: São Pedro e São Paulo
+            [{"POS": "DET"}, {"POS": "PROPN"}, {"POS": "ADP"}, {"POS": "PROPN"}],  # Ex: O Rio de Janeiro
+            [{"POS": "DET"}, {"POS": "NOUN"}, {"POS": "ADP"}, {"POS": "PROPN"}],  # Ex: A cidade de São Paulo
+            [{"POS": "DET"}, {"POS": "NOUN"}, {"POS": "ADP"}, {"POS": "PROPN"}, {"POS": "CCONJ"}, {"POS": "PROPN"}],  # Ex: A cidade de São Paulo e Rio de Janeiro
+        ]
+        for pattern in patterns:
+            self.matcher.add("Toponimo", [pattern])
+
+    @LoggingHandler.log_method("ToponymHandler", "lemmatize_and_extract_toponyms", False)
+    def lemmatize_and_extract_toponyms(self, text):
+        try:
+            doc = self.nlp(text)
+            lemmatized_text = " ".join([token.lemma_ for token in doc])
+            matches = self.matcher(doc)
+            toponyms = [doc[start:end].text for match_id, start, end in matches]
+            self.log_method("ToponymHandler", "lemmatize_and_extract_toponyms", True)
+            return lemmatized_text, ",".join(toponyms)
+        except Exception as e:
+            return "", ""
