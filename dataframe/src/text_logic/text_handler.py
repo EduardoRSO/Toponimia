cd04086f8491266e1dbff88eb5caaeaa09408b14
erso <edusillva784@gmail.com>
2024-10-02 12:03:41 -0300

    Implementei text_handler

---
+++ dataframe/src/text_logic/text_handler.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from logging_logic.logging_handler import LoggingHandler
+
+class TextHandler(LoggingHandler):
+    def __init__(self, directory_name):
+        super().__init__(directory_name)
+        self.directory_name = directory_name
+
+    @LoggingHandler.log_method("TextHandler", "extract_raw_text", True)    
+    def extract_raw_text(self, html_content):
+        try:
+            soup = BeautifulSoup(html_content, 'html.parser')
+            raw_text = soup.get_text(separator=' ', strip=True)
+            return raw_text
+        except Exception as e:
+            return "Erro ao extrair o texto bruto"
