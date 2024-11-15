28e65b42a9ce79942e2870cdf827f8ba98bff8cf
erso <edusillva784@gmail.com>
2024-11-15 15:29:39 -0300

    Mudei a main.py

--- classificacao/main.py
+++ classificacao/main.py
@@ -6,17 +6,18 @@ if __name__ == "__main__":
     classification_handler = ClassificationWorkflowHandler()
 
     # Ler o arquivo CSV
-    df = pd.read_csv('../dataframe/src/Alesp/Alesp_dataframe_part_1.csv',delimiter='|',encoding='utf-8').iloc[0]
+    df = pd.read_csv('dataframe/src/Alesp/Alesp_dataframe_part_1.csv', delimiter='|',encoding='utf-8')
     
     # Iterar sobre cada linha do DataFrame
     for index, row in df.iterrows():
         lemmatized_text = row['lemmatized_text']
-        extracted_toponyms = row['extracted_toponyms'].strip('[]').replace("'", "").split(', ')
-
+        extracted_toponyms = (row['extracted_toponyms']).split(',')
+        
         # Calcular a confiança de ser um toponímico para cada palavra extraída
+        print(f"Texto lematizado: '{lemmatized_text}'")
         for toponym in extracted_toponyms:
             confidence = classification_handler.calculate_toponym_confidence(toponym, lemmatized_text)
             # Imprimir o texto lematizado, a palavra e a porcentagem de confiança em linhas diferentes
-            print(f"Texto lematizado: '{lemmatized_text}'")
-            print(f"Palavra: '{toponym}'")
-            print(f"Porcentagem de confiança: {confidence:.2f}\n")
+            print(f"Palavra: '{toponym}', Porcentagem de confiança: {confidence:.2f}")
+        
+        break
